79eea787d9b6c1ba64629b0a86ec261abbea066b
Mohit7891 <mohitjain.cs23@gmail.com>
2023-07-15 21:13:27 +0530

    Final changes

--- server.js
+++ server.js
@@ -1,18 +1,21 @@
 const express=require('express');
 const app = express();
+const cors = require('cors');
 const port=process.env.PORT || 5000;
 const mondoDB=require('./db');
 mondoDB();
 const BASE_URL=process.env.BASE_URL
 
-app.use((req,res,next) => {
-    res.setHeader("Access-Control-Allow-Origin", `${BASE_URL}`);
-    res.header(
-        "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept"
-    );
-    next();
-})
+
+app.use(cors({origin :"*"}));
+// app.use((req,res,next) => {
+//   res.setHeader("Access-Control-Allow-Origin", `${BASE_URL}`);
+  //  res.header(
+        //"Access-Control-Allow-Headers",
+        //"Origin, X-Requested-With, Content-Type, Accept"
+    //);
+    //next();
+//})
 
 app.get('/',(req,res)=>{res.send('App is running');});
 
